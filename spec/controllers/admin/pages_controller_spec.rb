d8da79e8a9a2768deef14bbbd07976f538d46e7c
Bantik <corey@seologic.com>
2010-11-22 16:10:39 -0600

    Pages controller specs complete.

--- spec/controllers/admin/pages_controller_spec.rb
+++ spec/controllers/admin/pages_controller_spec.rb
@@ -10,48 +10,46 @@ describe Admin::PagesController do
   end
 
   it "show action should render show template" do
-    get :show, :site_id => @site.id, :id => @page.id
+    get :show, :site_id => @site.id.to_s, :id => @page.id
     response.should render_template(:show)
   end
   
   it "new action should render new template" do
-    get :new, :site_id => @site.id
+    get :new, :site_id => @site.id.to_s
     response.should render_template(:new)
   end
   
   it "create action should render new template when model is invalid" do
     Page.any_instance.stubs(:valid?).returns(false)
-    post :create, :site_id => @site.id
+    post :create, :site_id => @site.id.to_s
     response.should render_template(:new)
   end
 
   it "create action should redirect when model is valid" do
     Page.any_instance.stubs(:valid?).returns(true)
-    post :create, :site_id => @site.id
-    response.should redirect_to(admin_page_url(assigns[:page]))
+    post :create, :site_id => @site.id, :page => {:page_title => 'Foo', :content => 'Bar'}
+    response.should redirect_to(admin_site_url(@site))
   end
   
   it "edit action should render edit template" do
-    get :edit, :id => @page, :site_id => @site.id
+    get :edit, :id => @page.id, :site_id => @site.id
     response.should render_template(:edit)
   end
   
   it "update action should render edit template when model is invalid" do
-    Page.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => @page, :site_id => @site.id
+    put :update, :id => @page.id, :site_id => @site.id.to_s, :page => {:page_title => nil}
     response.should render_template(:edit)
   end
 
   it "update action should redirect when model is valid" do
-    Page.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => @page, :site_id => @site.id
-    response.should redirect_to(admin_page_url(assigns[:page]))
+    put :update, :id => @page.id, :site_id => @site.id.to_s, :page => {:desired_slug => 'foozball'}
+    response.should redirect_to(admin_site_url(@site))
   end
   
   it "destroy action should destroy model and redirect to index action" do
-    page = Page.first
-    delete :destroy, :id => page.id, :site_id => @site.id
-    response.should redirect_to(admin_pages_url)
-    Page.where(:id => page.id).first.should be_nil
+    delete :destroy, :id => @page.id, :site_id => @site.id.to_s
+    response.should redirect_to(admin_site_url(@site))
+    Page.where(:id => @page.id).first.should be_nil
   end
+
 end
