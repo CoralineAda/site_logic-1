400d43cba282625beb28255343d1a42ee58080fa
Bantik <corey@seologic.com>
2010-11-29 14:08:49 -0600

    Removed another TinyMCE vestige; don't capture nil sites in route file.

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,18 @@ Rails.application.routes.draw do
   class SiteConstraint
     def initialize; end
     def matches?(request)
-      Site.where(:domain => request.domain)
+      ! Site.where(:domain => request.domain).blank?
     end
   end
 
   constraints(SiteConstraint.new) do
-    root :to => "pages#show"
-    match ':page_slug/', :to => 'pages#show'
-    match ':page_slug(/:nested_slug)/', :to => 'pages#show'
+    root :to => "site_logic/pages#show"
+    match ':page_slug/', :to => 'site_logic/pages#show'
+    match ':page_slug(/:nested_slug)/', :to => 'site_logic/pages#show'
   end
   
   namespace :admin do
-    root :to => "admin/sites#index"
+    root :to => "site_logic/admin/sites#index"
     resources :sites do
       resources :pages
     end
