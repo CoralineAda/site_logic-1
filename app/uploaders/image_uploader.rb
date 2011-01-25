4f21a288ddd67cd817c24371f7ef5e3812641193
Roderick Monje <rod@seologic.com>
2011-01-25 14:25:17 -0600

    restored custom store_dir; removed obsolete storage setting (see initializers)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,16 +6,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   # include CarrierWave::ImageScience
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :grid_fs
-  # storage :s3
-#   storage Rails.application.config.file_storage
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-#   def store_dir
-#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-#   end
+  def store_dir
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
