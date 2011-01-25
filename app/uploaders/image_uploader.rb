c8673ef3176e9818779c665270658561fa07ae39
Roderick Monje <rod@seologic.com>
2011-01-25 11:28:11 -0600

    storage setting is defined in host app; reverted all other global settings to default

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,15 +7,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  # storage :grid_fs
   # storage :s3
-  storage Rails.application.config.file_storage
+#   storage Rails.application.config.file_storage
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+#   def store_dir
+#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -33,7 +33,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 #   version :thumb do
 #     process :scale => [50, 50]
 #   end
-
   version :content
   version :thumb
 
