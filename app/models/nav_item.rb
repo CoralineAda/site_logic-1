e029f3c4c5020807b1164a65802fb6483863e501
Bantik <corey@seologic.com>
2011-02-10 10:56:47 -0600

    Fix for children of non-primary-nav

--- app/models/nav_item.rb
+++ app/models/nav_item.rb
@@ -45,7 +45,7 @@ class NavItem
   # Instance methods ===============================================================================
 
   def children
-    self.site.nav_items.primary.select{|ni| ni.parent_id == self.id.to_s}.sort{|a,b| a.position.to_i <=> b.position.to_i}
+    self.site.nav_items.select{|ni| ni.parent_id == self.id.to_s && ni.kind != 'Dropdown'}.sort{|a,b| a.position.to_i <=> b.position.to_i}
   end
 
   def parent
