7e9572b30d082f47e65af332620c1bc78c478155
Roderick Monje <rod@seologic.com>
2011-06-08 14:39:51 -0500

    configure metric_fu only in test env

--- config/initializers/metric_fu.rb
+++ config/initializers/metric_fu.rb
@@ -1,8 +1,10 @@
-require 'metric_fu'
+if Rails.env.test?
+  require 'metric_fu'
 
-MetricFu::Configuration.run do |config|
-  config.metrics  = [:churn, :saikuro, :stats, :flog, :flay]
-  config.graphs   = [:flog, :flay, :stats]
-  config.rcov[:test_files] = ['spec/**/*_spec.rb']
-  config.rcov[:rcov_opts] << '-Ispec' # Needed to find spec_helper
+  MetricFu::Configuration.run do |config|
+    config.metrics  = [:churn, :saikuro, :stats, :flog, :flay]
+    config.graphs   = [:flog, :flay, :stats]
+    config.rcov[:test_files] = ['spec/**/*_spec.rb']
+    config.rcov[:rcov_opts] << '-Ispec' # Needed to find spec_helper
+  end
 end
