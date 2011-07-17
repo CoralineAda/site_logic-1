193ff64b61e441e1ba83e980b5f808b0afdd4fd4
Roderick Monje <rod@seologic.com>
2011-07-16 20:43:33 -0500

    new-page button is always displayed; fixed cucumber env

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,17 +22,6 @@ require 'capybara/session'
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
 
-# If you set this to false, any error raised from within your app will bubble
-# up to your step definition and out to cucumber unless you catch it somewhere
-# on the way. You can make Rails rescue errors and render error pages on a
-# per-scenario basis by tagging a scenario or feature with the @allow-rescue tag.
-#
-# If you set this to true, Rails will rescue all errors and render error
-# pages, more or less in the same way your application would behave in the
-# default production environment. It's not recommended to do this for all
-# of your scenarios, as this makes it hard to discover errors in your application.
-ActionController::Base.allow_rescue = false
-
 # How to clean your database when transactions are turned off. See
 # http://github.com/bmabey/database_cleaner for more info.
 if defined?(ActiveRecord::Base)
