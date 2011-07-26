49a920a603af315295ca122d608be0ea371d5c8e
Roderick Monje <rod@seologic.com>
2011-07-26 14:22:26 -0500

    made all routes RESTful; removed redundant string interpolation

--- config/routes.rb
+++ config/routes.rb
@@ -11,25 +11,25 @@ Rails.application.routes.draw do
     def matches?(request)
       return false if request.subdomain == 'admin'
       site = Site.where(:domain => request.host).first
-      site && site.redirects.where(:source_url => "#{request.path}").first
+      site && site.redirects.where(:source_url => request.path).first
     end
   end
 
   constraints(RedirectConstraint.new) do
-    match ':source_url', :to => 'redirects#show'
+    get ':source_url', :to => 'redirects#show'
   end
 
   constraints(SiteConstraint.new) do
-    root :to => 'pages#show'
-    match '*path' => 'pages#show'
+    root :to => 'pages#show', :via => 'get'
+    get '*path' => 'pages#show'
   end
 
   namespace :admin do
-    match 'sites/:site_id/nav_items/reorder', :to => 'nav_items#reorder', :as => 'site_reorder_nav_items'
+    put 'sites/:site_id/nav_items/reorder', :to => 'nav_items#reorder', :as => 'site_reorder_nav_items'
     resources :sites do
       resources :nav_items
       resources :pages do
-        match 'preview', :to => 'pages#preview', :as => 'preview_admin_site_page'
+        get 'preview', :to => 'pages#preview', :as => 'preview_admin_site_page'
       end
       resources :redirects
     end
