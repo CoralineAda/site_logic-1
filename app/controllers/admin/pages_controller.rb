d0506dc8b3c70d941d20ac8462d7d5657640a310
Roderick Monje <rod@seologic.com>
2011-09-27 12:16:54 -0500

    page-create/update/destroy redirects to the site if more than one exists; extracted redirect_after_write; removed redundant parentheses

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -26,14 +26,14 @@ class Admin::PagesController < ApplicationController
 
   def create
     if params[:commit] == 'Preview'
-      @page = @site.pages.new(params[:page])
+      @page = @site.pages.new params[:page]
       render :action => 'preview', :layout => @site.layout
     else
-      @page = @site.pages.create(params[:page])
+      @page = @site.pages.create params[:page]
       if @page.valid?
         @page.publish! if params[:page][:state] == 'Published'
-        flash[:notice] = "Successfully created the page."
-        if params[:page][:create_navigation_item] == "true"
+        flash[:notice] = 'Successfully created the page.'
+        if params[:page][:create_navigation_item] == 'true'
           redirect_to new_admin_site_nav_item_path(
             @site,
             :nav_item => {
@@ -44,7 +44,7 @@ class Admin::PagesController < ApplicationController
             :creating_page => true
           )
         else
-          redirect_to admin_site_pages_path(@site)
+          redirect_after_write
         end
       else
         render :action => 'new'
@@ -57,14 +57,14 @@ class Admin::PagesController < ApplicationController
 
   def update
     if params[:commit] == 'Preview'
-      @page = @site.pages.new(params[:page])
+      @page = @site.pages.new params[:page]
       render :action => 'preview', :layout => @page.site.layout
     else
-      if @page.update_attributes(params[:page])
+      if @page.update_attributes params[:page]
         @page.publish! if params[:page][:state] == 'Published'
         @page.unpublish! if params[:page][:state] == 'Draft'
-        flash[:notice] = "Successfully updated the page."
-        redirect_to admin_site_pages_path(@site)
+        flash[:notice] = 'Successfully updated the page.'
+        redirect_after_write
       else
         render :action => 'edit'
       end
@@ -73,23 +73,26 @@ class Admin::PagesController < ApplicationController
 
   def destroy
     @page.destroy
-    flash[:notice] = "Successfully destroyed the page."
-    redirect_to admin_site_pages_path(@site)
+    flash[:notice] = 'Successfully destroyed the page.'
+    redirect_after_write
   end
 
   def preview
-    @page = @site.pages.new(params[:page])
+    @page = @site.pages.new params[:page]
     render :layout => @page.site.layout
   end
 
   private
 
+  def redirect_after_write
+    redirect_to Site.count > 1 ? admin_site_path(@site) : admin_site_pages_path(@site)
+  end
+
   def scope_site
-    @site = Site.find(params[:site_id])
+    @site = Site.find params[:site_id]
   end
 
   def scope_page
-    @page = @site.pages.find(params[:id])
+    @page = @site.pages.find params[:id]
   end
-
 end
