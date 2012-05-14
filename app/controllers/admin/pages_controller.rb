6598046b7c6792d376a40d116cf922d38949d7d1
Roderick Monje <rod@seologic.com>
2012-05-14 13:45:00 -0500

    pages C no longer authenticates (conflicts with ckeditor upload)

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,5 +1,4 @@
 class Admin::PagesController < ApplicationController
-  before_filter :authenticate_user! if Object.const_defined?('Devise')
   before_filter :scope_site
   before_filter :scope_page, :only => [:edit, :update, :destroy, :show]
 
